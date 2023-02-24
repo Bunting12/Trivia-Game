2b590b83ff435f540a9f9fcd5128c54126a7fb0f
davidbunting <david@pocketsquaredevelopment.com>
2023-02-24 07:33:57 +0100

    Initial Commit

---
+++ TriviaGame/TriviaGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaGameApp.swift
+//  TriviaGame
+//
+//  Created by David Bunting on 24.02.23.
+//
+
+import SwiftUI
+
+@main
+struct TriviaGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
